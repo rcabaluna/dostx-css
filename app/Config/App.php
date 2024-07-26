7aa9843f8a64b3b963080074c7aefd8ebf44de14
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2024-07-26 13:35:22 +0800

    for testing

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = BASE;
+    public string $baseURL = BASE.'/dostx_css';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
